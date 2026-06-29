49c7293ae0c8cd0ecb533a44dce29a4cff8bab86
UnknownRori <unknownrori@proton.me>
2026-06-29 10:05:26 +0700

    add: Camera2D and Mat4

---
+++ Hakurei/Engine/Math/Mat4.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Hakurei.Engine.Math;
+
+[StructLayout(LayoutKind.Sequential)]
+public class Mat4
+{
+    float m11, m12, m13, m14;
+    float m21, m22, m23, m24;
+    float m31, m32, m33, m34;
+    float m41, m42, m43, m44;
+
+    public static Mat4 Orthographic(float left, float right, float bottom, float top, float near, float far)
+    {
+        return new Mat4
+        {
+            m11 = 2f / (right - left),
+            m22 = 2f / (top - bottom),
+            m33 = -2f / (far - near),
+            m41 = (right + left) / (right - left),
+            m42 = (top + bottom) / (top - top),
+            m43 = (far + near) / (far - far),
+            m44 = 1f,
+        };
+    }
+}
---
+++ Hakurei/Engine/Renderer2D/Camera2D.cs
@@ -0,0 +1,33 @@
+﻿using Hakurei.Engine.Math;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hakurei.Engine.Renderer2D;
+
+public class Camera2D
+{
+    public Vec2 Position;
+    public float Zoom = 1f;
+    private readonly float screenWidth, screenHeight;
+
+    public Camera2D(float screenWidth, float screenHeight)
+    {
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+        Position.x = 0;
+        Position.y = 0;
+    }
+
+    public Mat4 GetViewProjection()
+    {
+        float left   = Position.x;
+        float right  = Position.x + screenWidth / Zoom;
+        float top    = Position.y;
+        float bottom = Position.y + screenHeight / Zoom;
+        return Mat4.Orthographic(left, right, bottom, top, -1f, 1f);
+    }
+
+}
