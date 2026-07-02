001f64111e7ea0315df9047d83b40223f8e04df8
UnknownRori <unknownrori@proton.me>
2026-07-02 14:38:10 +0700

    add: game project

--- Hakurei/Program.cs
+++ Hakurei/Program.cs
@@ -1,11 +1,10 @@
-﻿using Hakurei.Game;
-using SDL3;
+﻿using Hakurei.Engine;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Game game = new Game();
+        App game = new App("urmom", 800, 600, (int) SDL3.SDL.WindowFlags.AlwaysOnTop);
         game.Run();
     }
 }
---
+++ Shoot Down Hakurei/Program.cs
@@ -0,0 +1,11 @@
+﻿using Hakurei.Engine;
+namespace Shoot_Down_Hakurei;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        App app = new App("urmom", 800, 600, 0);
+        app.Run();
+    }
+}
